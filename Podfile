b8bf20e90cd4687cd64a5cc6ebc0ab515ac6053f
vi <zhang2011@outlook.com>
2018-04-13 20:16:29 +0800

    init

---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '11.0'
+
+def pods
+    pod 'AVOSCloud'
+end
+
+target :'RedJ' do
+    pods
+end
---
+++ RedJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RedJ
+//
+//  Created by vi~ on 2018/4/13.
+//  Copyright © 2018年 zhangyangwei.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
